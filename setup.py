0c7026386b4c0848f29e7708f8d377842be9b4f8
jona <yablee@gmail.com>
2022-03-11 18:31:11 +0100

    Bump version number and add readme.rst as long description for pypi

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup
 
+with open("readme.rst", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='ikarus',
-    version='0.0.1',
+    version='0.0.2',
     packages=['ikarus'],
     desription='Machine Learning classifier of tumor cells',
     install_requires=[
