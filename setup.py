719d80cb4e083b2067115d4c711072934a850615
Jan Dohmen <38068883+dohmjan@users.noreply.github.com>
2022-04-20 11:47:06 +0200

    add license to setup

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ setup(
     version='0.0.2',
     packages=['ikarus'],
     desription='Machine Learning classifier of tumor cells',
+    license='MIT',
     install_requires=[
         'numpy',
         'pandas',
